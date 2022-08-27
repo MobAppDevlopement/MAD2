dfce9340055dd92be8ab2b49e926a79e0625dd38
SAUMIL PATEL <devilsaumil11@gmail.com>
2022-08-27 19:33:20 +0530

    P2

--- app/src/main/java/com/example/madpractical_2_20012011124/MainActivity.kt
+++ app/src/main/java/com/example/madpractical_2_20012011124/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.madpractical_2_20012011124
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    val TAG="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showMessage("onCreate function called")
+    }
+    private fun showMessage(msg:String){
+        Log.i(TAG,msg)
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.main_constraint_layout),msg,Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        showMessage("onResume function called.")
+        super.onResume()
     }
 }
