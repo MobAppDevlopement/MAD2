ebf48e13461d53194c52055f5e73d9ea55b791e2
SAUMIL PATEL <devilsaumil11@gmail.com>
2022-08-31 11:39:33 +0530

    P2

--- app/src/main/java/com/example/madpractical_2_20012011124/MainActivity.kt
+++ app/src/main/java/com/example/madpractical_2_20012011124/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
+import javax.net.ssl.SNIServerName
 
 class MainActivity : AppCompatActivity() {
     val TAG="MainActivity"
@@ -22,5 +23,34 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         showMessage("onResume function called.")
         super.onResume()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG,"onRestart function called")
+        Toast.makeText(this,"onRestart function called",Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.main_constraint_layout),"onRestart function called",Snackbar.LENGTH_SHORT).show()
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG,"onPause function called")
+        Toast.makeText(this,"onPause function called",Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.main_constraint_layout),"onPause function called",Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG,"onPause function called")
+        Toast.makeText(this,"onPause function called",Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.main_constraint_layout),"onPause function called",Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG,"onDestroy function called")
+        Toast.makeText(this,"onDestroy function called",Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.main_constraint_layout),"onDestroy function called",Snackbar.LENGTH_SHORT).show()
     }
 }
